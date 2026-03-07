6e6ff18847f80c91a95271334750826fba7532ff
ankoholik <nesp.alexey@gmail.com>
2026-03-07 15:04:11 +0300

    gestures: add gesture registry

---
+++ airmouse/gestures/__init__.py
@@ -0,0 +1,3 @@
+from .registry import GESTURES
+
+__all__ = ["GESTURES"]
---
+++ airmouse/gestures/registry.py
@@ -0,0 +1,9 @@
+"""Built-in gesture class labels (model output IDs)."""
+
+GESTURES: dict[int, str] = {
+    1: "no action",
+    2: "cursor",
+    3: "left click",
+    4: "right click",
+    5: "scrolling",
+}
